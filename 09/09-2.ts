16768c22a60f554b567e0e281f6f3285ef7e1a86
Lukas Polak <admin@lukaspolak.cz>
2023-12-09 14:18:22 +0100

    09

---
+++ 09/09-2.ts
@@ -0,0 +1,59 @@
+import { loadInput, writeOutput, compareTestFiles } from "../template/utils";
+
+const solve = (realInput: boolean, solutionId: string, expected: number) => {
+    let input: string[] = loadInput(solutionId, realInput);
+
+    const seqs: Sequence[] = [];
+
+    type Sequence = {
+        id: number;
+        history: number[][];
+    };
+
+    let i = 0;
+    for (const line of input) {
+        seqs.push({
+            id: i,
+            history: [line.split(" ").map((e) => Number(e))]
+        });
+        i++;
+    }
+
+    for (const seq of seqs) {
+        let run = 0;
+        while (seq.history[run].find((e) => e !== 0)) {
+            const newSeq = [];
+            for (let y = 0; y < seq.history[run].length - 1; y++) {
+                newSeq.push(seq.history[run][y + 1] - seq.history[run][y]);
+            }
+            seq.history.push(newSeq);
+            run++;
+        }
+    }
+
+    const final = [];
+
+    for (const seq of seqs) {
+        seq.history[seq.history.length - 1].unshift(0);
+        for (let i = seq.history.length - 2; i >= 0; i--) {
+            const lower = seq.history[i + 1];
+            seq.history[i].unshift(seq.history[i][0] - lower[0]);
+        }
+        final.push(seq.history[0][0]);
+    }
+
+    const initialValue = 0;
+    const output = final.reduce(
+        (accumulator, currentValue) => accumulator + Number(currentValue),
+        initialValue,
+    );
+
+    console.log(output);
+    if (Number(output) === expected) {
+        console.log("OK");
+    } else {
+        console.error("Wrong");
+    }
+};
+
+solve(false, "09", 2);
